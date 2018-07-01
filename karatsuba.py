fb495bf7b65e95f58216d91970ee18810ada662a
AgAnkitGawde <gawde17@gmail.com>
2018-06-30 18:36:40 -0700

    recursive

--- karatsuba.py
+++ karatsuba.py
@@ -1,4 +1,21 @@
-import pandas as pd
-import numpy as np
-print("a")
-print("b")
+def karat(x,y):
+    if len(str(x)) == 1 or len(str(y)) == 1:
+        return x*y
+    else:
+        n = max(len(str(x)),len(str(y)))
+        n2 = n // 2
+        a = x // 10**(n2)
+        b = x % 10**(n2)
+        c = y // 10**(n2)
+        d = y % 10**(n2)
+
+        z0 = karat(b,d)
+        z1 = karat((a+b),(c+d))
+        z2 = karat(a,c)
+
+        return (z2 * 10**(2*n2)) + ((z1 - z2 - z0) * 10**(n2)) + (z0)
+
+x,y=input().split()
+x=int(x)
+y=int(y)
+print(karat(x,y))
